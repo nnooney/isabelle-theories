76913f55abf224c6beeefa01ba57ad18b158c882
Nicholas Nooney <nicholasnooney@gmail.com>
2022-10-21 12:51:05 -0700

    Initial Chapter 3 work

--- concrete-semantics/chapter3.thy
+++ concrete-semantics/chapter3.thy
@@ -54,6 +54,61 @@ lemma "aval (asimp a) s = aval a s"
   done
 
 (* 3.1 *)
+fun optimal :: "aexp \<Rightarrow> bool" where
+  "optimal (N n) = True" |
+  "optimal (V x) = True" |
+  "optimal (Plus (N n1) (N n2)) = False" |
+  "optimal (Plus a1 a2) = ((optimal a1) \<and> (optimal a2))"
 
+theorem asimp_const_optimal: "optimal (asimp_const a)"
+  apply (induction a)
+  apply (auto split: aexp.split)
+  done
+
+(* 3.2 *)
+fun full_plus :: "aexp \<Rightarrow> aexp \<Rightarrow> aexp" where
+  (* Ways to combine two constants *)
+  "full_plus (N n1) (N n2) = N (n1 + n2)" |
+  "full_plus (Plus (N n1) a) (N n2) = Plus a (N (n1 + n2))" |
+  "full_plus (Plus a (N n1)) (N n2) = Plus a (N (n1 + n2))" |
+  "full_plus (N n1) (Plus (N n2) a) = Plus a (N (n1 + n2))" |
+  "full_plus (N n1) (Plus a (N n2)) = Plus a (N (n1 + n2))" |
+  (* Ways to shift constants right *)
+  "full_plus (Plus a1 (N n1)) a2 = Plus (Plus a1 a2) (N n1)" |
+  "full_plus a1 (Plus a2 (N n1)) = Plus (Plus a1 a2) (N n1)" |
+  "full_plus (Plus (N n1) a1) (Plus (N n2) a2) = Plus (Plus a1 a2) (N (n1 + n2))" |
+  (* Everything else *)
+  "full_plus a1 a2 = Plus a1 a2"
+
+lemma aval_full_plus: "aval (full_plus a1 a2) s = aval a1 s + aval a2 s"
+  apply (induction rule: full_plus.induct)
+  apply auto
+  done
+
+fun full_asimp :: "aexp \<Rightarrow> aexp" where
+  "full_asimp (N n) = N n" |
+  "full_asimp (V x) = V x" |
+  "full_asimp (Plus a1 a2) = full_plus (full_asimp a1) (full_asimp a2)"
+
+value "full_asimp (Plus (N 1) (Plus (V ''x'') (N 2)))"
+value "full_asimp (Plus (N (- 2)) (Plus (N (- 1)) (V [])))"
+
+lemma "aval (full_asimp a) s = aval a s"
+  apply (induction a)
+  apply (auto simp: aval_full_plus)
+  done
+
+(* Great, we preserved that it doesn't change evaluation, but can we
+  prove that it's also optimal?  *)
+
+fun count_n :: "aexp \<Rightarrow> int" where
+  "count_n (N n) = 1" |
+  "count_n (V x) = 0" |
+  "count_n (Plus a1 a2) = (count_n a1) + (count_n a2)"
+
+(* Adding more cases to full_plus is making the evaluation take awhile. There has
+  to be a better way to write the simplification rules for full_plus. *)
+theorem full_asimp_optimal: "count_n (full_asimp a) \<le> 1"
+  oops
 
 end
